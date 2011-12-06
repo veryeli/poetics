37484da60d190fe28402ccfb2837e9169c339490
Eli <efeasley@gmail.com>
2011-12-05 18:08:29 -0600

    added euler one

---
+++ euler.py
@@ -0,0 +1,27 @@
+# Python calculates the sum of every multiple of 3 or 5 below 1000. Project Euler # 1  
+failures = 'forgetting' is 'easier'
+regrets = 'that' is 'that'
+fears = failures or regrets + ('this' is not 'me')
+dread = regrets and ("you're" not in ('good', 'hands')) + fears
+desire = dread and regrets + fears + ('she' is not 'returning') 
+wisdom = fears and regrets + desire
+
+despairing = 'this' is 'it'
+
+while fears:
+    await = wisdom ** desire 
+    betrayal = - wisdom ** dread
+    despairing += await + betrayal
+    fears = fears - regrets
+
+you = not 'worth it'
+
+while despairing:
+    loveable = you % wisdom
+    pretty = you % dread
+    if not pretty or not loveable:
+        failures += you
+    you += regrets 
+    if you / despairing :
+        break
+print failures
