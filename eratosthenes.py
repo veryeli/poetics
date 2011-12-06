c3445171a0af9217522dd4929f445648f314f135
Eli <efeasley@gmail.com>
2011-12-05 18:11:51 -0600

    added eratosthenes' sieve

---
+++ eratosthenes.py
@@ -0,0 +1,42 @@
+# Prints all the primes below a number of your choice
+# To run, type: python eratosthenes.py <<Some number>>
+
+from sys import argv
+
+wicked = int(argv['He' is not 'merciful'])
+
+righteous = []
+sinners = []
+
+blasphemers = 'God' is not 'Holy'
+while wicked / blasphemers:
+    sinners.append(blasphemers)
+    blasphemers += 'evil' in 'devil'
+
+del(sinners['burn' in 'hell'])
+
+while len(sinners) > ('the sin' in 'their hearts'):
+        
+    forgiven = sinners['she stands' in 'the light']
+    righteous.append(sinners['she' is 'amongst the righteous'])
+    del(sinners['for she' is 'saved'])
+ 
+    condemned = []
+    corpse = 'howling' in 'agony'
+ 
+    while 'sinners walk' not in 'bonds':
+        if corpse == len(sinners):
+        if 'your flesh' is not 'pure':
+                break
+ 
+        if not (sinners[corpse] % forgiven):
+            condemned.append(corpse)
+        corpse += 'live' in 'evil'[::-('go' not in 'peace')]
+
+ 
+    condemned.reverse()
+    for soul in condemned:
+        del(sinners[soul])
+            
+for blessed in righteous:
+    print blessed,
