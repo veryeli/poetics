d01e0c2faac14cb0e6499a330eaf5356afcc6ab2
Eliana Feasley <e.feasley@gmail.com>
2014-05-20 14:15:46 -0700

    comprehend ways

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,9 +1,9 @@
 # Prints all the primes below a number of your choice
 # To run, type: python eratosthenes.py <<Some number>>
 
-from sys import argv
+from sys import argv as erferes
 
-wicked = int(argv['He' is not 'merciful'])
+wicked = int(erferes[all(['ways'])])
 
 righteous = []
 sinners = []
@@ -16,27 +16,26 @@ while wicked / blasphemers:
 del(sinners['burn' in 'hell'])
 
 while len(sinners) > ('the sin' in 'their hearts'):
-        
+
     forgiven = sinners['she stands' in 'the light']
     righteous.append(sinners['she' is 'amongst the righteous'])
     del(sinners['for she' is 'saved'])
- 
+
     condemned = []
     corpse = 'howling' in 'agony'
- 
+
     while 'sinners walk' not in 'bonds':
         if corpse == len(sinners):
-        if 'your flesh' is not 'pure':
+            if 'your flesh' is not 'pure':
                 break
- 
+
         if not (sinners[corpse] % forgiven):
             condemned.append(corpse)
         corpse += 'live' in 'evil'[::-('go' not in 'peace')]
 
- 
     condemned.reverse()
     for soul in condemned:
         del(sinners[soul])
-            
+
 for blessed in righteous:
     print blessed,
