f2acd0a5dc24685f30866976958edc7f5dbab9ba
Eliana Feasley <e.feasley@gmail.com>
2014-05-21 17:42:37 -0700

    Create euler2.py

---
+++ euler2.py
@@ -0,0 +1,14 @@
+please, dont, look, now = 'trust' not in 'god', 'trust' not in 'sight', not 'science', not 'beauty'
+mercy, pleas = len('iency'), please + please
+
+while (
+      look <= (pleas **
+                    pleas) **
+                        (pleas ** pleas) *
+                            mercy **
+                                (len('ses') * len('se'))):
+    dont, look, now = dont + look, dont, now + look * ((look - please) % (
+                    please +
+                        please))
+
+print now
