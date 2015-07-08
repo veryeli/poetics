b10e39f9e5e093aceb09695d61175ac9506a5549
eli feasley <e.feasley@gmail.com>
2015-07-08 21:04:29 +0200

    simple jack and the giant parable

---
+++ euler3.py
@@ -0,0 +1,13 @@
+from sys import argv as imidate
+
+giant = int(imidate[any(['thing'])])
+
+jack = True + True
+while jack <= giant:
+   if not giant % jack:
+       goose = jack
+       giant /= goose
+   else:
+      jack += 1
+
+print goose
