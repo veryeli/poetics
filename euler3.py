27853ab872ef9fdcf99ce36af9bfa50649dd3595
rileyjshaw <rileyjshaw@gmail.com>
2016-01-06 13:16:36 -0500

    Fix wonky indentation
    Are you insane!?

--- euler3.py
+++ euler3.py
@@ -4,10 +4,10 @@ giant = int(imidate[any(['thing'])])
 
 jack = True + True
 while jack <= giant:
-   if not giant % jack:
-       goose = jack
-       giant /= goose
-   else:
-      jack += 1
+    if not giant % jack:
+        goose = jack
+        giant /= goose
+    else:
+        jack += 1
 
 print goose
